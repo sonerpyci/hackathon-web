56941dd0d38e6c718be483e4ce35fb2806cda824
Kaan <ksonmezoz@outlook.com>
2019-03-22 23:55:36 +0300

    Auto wired

--- src/main/java/com/sonerpyci/ciceksepeti/hackathon/business/Business.java
+++ src/main/java/com/sonerpyci/ciceksepeti/hackathon/business/Business.java
@@ -3,17 +3,22 @@ package com.sonerpyci.ciceksepeti.hackathon.business;
 import com.sonerpyci.ciceksepeti.hackathon.business.entities.Gift;
 import com.sonerpyci.ciceksepeti.hackathon.business.entities.Order;
 import com.sonerpyci.ciceksepeti.hackathon.business.entities.Receiver;
+import com.sonerpyci.ciceksepeti.hackathon.services.GiftService;
 import com.sonerpyci.ciceksepeti.hackathon.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Business {
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private GiftService giftService;
+
     public Business(){}
 
     public List<Order> getAllOrders(){
-        OrderService orderService = new OrderService();
-
         List<Order> orders = orderService.findAllOrders().stream().map(order -> {
             Gift gift = new Gift(order.getGift().getId(), order.getGift().getName(), order.getGift().getImageSource(),
                     order.getGift().getType(), order.getGift().getAdditionalInfo());
