bcd89c4baf726bcc12ece1c88ae07775a217072a
Soner Paycı <sonerpayci@gmail.com>
2019-03-22 23:16:49 +0300

    created_at Column özellikleri değiştirildi

--- src/main/java/com/sonerpyci/ciceksepeti/hackathon/models/Order.java
+++ src/main/java/com/sonerpyci/ciceksepeti/hackathon/models/Order.java
@@ -1,6 +1,8 @@
 package com.sonerpyci.ciceksepeti.hackathon.models;
 
 
+import org.springframework.data.annotation.CreatedDate;
+
 import javax.persistence.*;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -21,8 +23,7 @@ public class Order {
     @ManyToOne
     private Receiver receiver;
 
-    @Column(name = "created_at")
-    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="created_at", columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private Date creationTime;
 
     public Receiver getReceiver() {
@@ -55,7 +56,7 @@ public class Order {
         return dateFormat.format(creationTime);
     }
 
-    public void setCreationTime() {
+    public void setCreationTime(Date newDate) {
         this.creationTime = new Date();
     }
 
