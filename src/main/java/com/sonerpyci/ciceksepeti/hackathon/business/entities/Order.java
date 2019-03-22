ec6a35a3ec9b32aeffda1885d0053e5a43f726f7
Kaan <ksonmezoz@outlook.com>
2019-03-22 23:12:01 +0300

    Business layer added

--- src/main/java/com/sonerpyci/ciceksepeti/hackathon/business/entities/Order.java
+++ src/main/java/com/sonerpyci/ciceksepeti/hackathon/business/entities/Order.java
@@ -2,5 +2,32 @@ package com.sonerpyci.ciceksepeti.hackathon.business.entities;
 
 public class Order {
     private long id;
-    private
-}
+    private Gift gift;
+    private Receiver receiver;
+
+    public Receiver getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(Receiver receiver) {
+        this.receiver = receiver;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Gift getGift() {
+        return gift;
+    }
+
+    public void setGiftId(Gift gift) {
+        this.gift = gift;
+    }
+
+
+    }
